2a6eaf8e619d94620fa8bb3d00b13fd8c2cfe962
czahie <czahie1996@gmail.com>
2018-12-25 13:03:51 -0500

    add requisite codes

---
+++ percolation/Percolation.java
@@ -0,0 +1,42 @@
+/* *****************************************************************************
+ *  Name: Zhe Cai
+ *  Date: 2018/12/25
+ *  Description: Algorithms-part 1 Assignment 1
+ **************************************************************************** */
+
+public class Percolation {
+
+    private boolean[][] grid; // open = True, blocked = False;
+    private int n; // row, col = n
+
+    /**
+     * Constructs a n by n grid, with all sites blocked.
+     * */
+    public Percolation(int n) {
+
+    }
+
+    public void open(int row, int col) {
+
+    }
+
+    public boolean isOpen(int row, int col) {
+
+    }
+
+    public boolean isFull(int row, int col) {
+
+    }
+
+    public int numberOfOpenSites() {
+
+    }
+
+    public boolean percolates() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ percolation/PercolationStats.java
@@ -0,0 +1,31 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class PercolationStats {
+
+    public PercolationStats(int n, int trials) {
+
+    }
+
+    public double mean() {
+
+    }
+
+    public double stddev() {
+
+    }
+
+    public double confidenceLo() {
+
+    }
+
+    public double confidenceHi() {
+        
+    }
+    public static void main(String[] args) {
+
+    }
+}
